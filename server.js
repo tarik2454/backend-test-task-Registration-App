35039c84227f8fc304868eec0973489153996f88
Taras Kovalyk <tarik2454@gmail.com>
2024-05-22 12:45:12 +0300

    adds process.env

--- server.js
+++ server.js
@@ -2,17 +2,12 @@ import mongoose from 'mongoose';
 
 import app from './app.js';
 
-const DB_HOST =
-  'mongodb+srv://tarik2454:Mongodb-voina2015@cluster0.nbrpxs5.mongodb.net/backend-test-task-Registration-App?retryWrites=true&w=majority&appName=Cluster0';
-
-const PORT = 3000;
-
 mongoose
-  .connect(DB_HOST, {
+  .connect(process.env.DB_HOST, {
     writeConcern: { w: 'majority' },
   })
   .then(() => {
-    app.listen(PORT, () => {
+    app.listen(3000, () => {
       console.log(
         'Database connection successful. Server running. Use our API on port: 3000'
       );
